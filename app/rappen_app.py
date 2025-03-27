202676cca48a0223968b1636e1979f6a4f1c349f
k5e <info@k5e.de>
2025-03-27 21:47:22 +0100

    Tageswähler

--- app/rappen_app.py
+++ app/rappen_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import plotly.express as px
+import datetime as dt
 
 st.set_page_config(
     page_title="Solar-Dashboard",
@@ -17,11 +18,21 @@ with st.sidebar:
     st.title('☀️ Solar-Dashboard')
     
     day_list = list(df_reshaped.DATE.unique())[::-1]
+    day_list.sort()
      
-    # selected_day = st.selectbox('Tag auswählen', day_list, index=len(day_list)-1)
-    # df_selected_day = df_reshaped[df_reshaped.DATE == selected_day]
+    selected_day = st.selectbox('Tag auswählen', day_list, index=len(day_list)-1)
+    day_index = day_list.index(selected_day)
+    if day_index > len(day_list)-7:
+        day_index = len(day_list)-7
+        
+    selected_days = list()
+    
+    for d in range(0, 7):
+        selected_days.append(day_list[day_index+d]) 
+    
+    df_selected_days = df_reshaped[df_reshaped.DATE.isin(selected_days)]
 
-    color_theme_list = ['plasma', 'blues', 'cividis', 'greens', 'inferno', 'magma', 'reds', 'rainbow', 'turbo', 'viridis']
+    color_theme_list = ['magma', 'plasma', 'blues', 'cividis', 'greens', 'inferno', 'reds', 'rainbow', 'turbo', 'viridis']
     selected_color_theme = st.selectbox('Farben auswählen', color_theme_list)
 
 def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
@@ -47,5 +58,8 @@ col = st.columns((1), gap='medium')
 with col[0]:
     st.markdown('## Energieertrag')
        
-    heatmap = make_heatmap(df_reshaped, 'DATE', 'TIME', 'AC_POWER', selected_color_theme)
+    heatmap = make_heatmap(df_selected_days, 'DATE', 'TIME', 'AC_POWER', selected_color_theme)
     st.altair_chart(heatmap, use_container_width=True)
+    
+   # st.subheader('Raw data')
+   # st.write(selected_days)
